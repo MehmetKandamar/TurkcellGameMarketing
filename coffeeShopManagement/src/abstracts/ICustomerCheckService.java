8dfc9f2dd184a908cc3c262ce382b5a94a1f4773
MehmetKandamar <mehmet.kandamar@outlook.com>
2022-02-18 09:52:23 +0300

    Delete coffeeShopManagement directory

--- coffeeShopManagement/src/abstracts/ICustomerCheckService.java
+++
@@ -1,9 +0,0 @@
-package abstracts;
-
-import entities.Customer;
-
-public interface ICustomerCheckService {
-	
-	boolean CheckIfRealPerson(Customer customer) throws Exception;
-
-}
--- coffeeShopManagement/src/abstracts/ICustomerService.java
+++
@@ -1,8 +0,0 @@
-package abstracts;
-
-import entities.Customer;
-
-public interface ICustomerService {
-	void Save(Customer customer) throws Exception;
-
-}
